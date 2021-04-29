bc961a6c8eafdfb2f707d94230802d122272d60b
edmarsantos <39056557+edmarsantos@users.noreply.github.com>
2021-04-29 08:13:56 -0300

    Lendo arquivo TXT e criando lista de objetos aula 3

--- Arquivos/src/arquivo/LerArquivoTxt.java
+++ Arquivos/src/arquivo/LerArquivoTxt.java
@@ -3,6 +3,8 @@ package arquivo;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivoTxt {
@@ -16,6 +18,9 @@ public class LerArquivoTxt {
 		//LEITURA DO ARQUIVO PARA UTILIZAR A FRENTE E A CODIFICAÇÃO UTILIZADA NO CASO "UTF-8"
 		Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
 		
+		//criando lsta de pessoas para mais a frente utilizar para adicionar as pessoas do regsitro lido
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
 		
 		//while para ler linha a linha enquanto houver registro
 		while(lerArquivo.hasNext()) {
@@ -25,13 +30,25 @@ public class LerArquivoTxt {
 						
 			//if para linha for null ou vazia ele nao vai mostrar server para eliminar possivies sujeiras do layout
 			if(linha!=null && !linha.isEmpty()) {
-				System.out.println(linha);	
+				//System.out.println(linha);
+				String[] dados = linha.split("\\;");
+				
+				//capturando os dados do registro
+				Pessoa pessoa = new Pessoa();
+				pessoa.setNome(dados[0]);
+				pessoa.setEmail(dados[2]);
+				pessoa.setIdade(Integer.parseInt(dados[1]));
+				
+				//adicionando na lista de pessoas
+				pessoas.add(pessoa);
+				
 			}
 				
 			
 		}
 		
-		
+		for(Pessoa pessoa : pessoas) //com esse for pode salvar no banco de dados ou gerar email
+		System.out.println(pessoa);
 		
 
 	}
